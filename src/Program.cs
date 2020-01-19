46c7a32fad04c298464ebf7f637d926b7e9eb4cb
furukawa <nkc.takashi.furukawa@gmail.com>
2020-01-20 00:35:52 +0900

    dotnet new

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
