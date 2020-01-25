c11f4461be288cddabafed77e11ffa8298df3a96
furukawa <nkc.takashi.furukawa@gmail.com>
2020-01-25 19:19:26 +0900

    sumiにsetup.shとsumi_buildを移動

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-local_path=`pwd`
-
-echo -e "\n# sumi" >> ~/.bash_profile
-echo -e "export PATH=${local_path}:\${PATH}" >> ~/.bash_profile
-source ~/.bash_profile
-
-echo "setup completed sumi."
